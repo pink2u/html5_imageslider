b923e5b81f65a7a48b5da0ef1c7c477a0f66153d
kanghyunsook <ppink2u@naver.com>
2019-01-20 16:07:28 +0900

    type2 method chainning added

--- HT502-imageSlider_template/js/type2.js
+++ HT502-imageSlider_template/js/type2.js
@@ -6,8 +6,16 @@ $(document).ready(function(){
     // $("#slider ul li:nth-child(3)").hide();
 
     setInterval(function(){
-        $("#slider ul li:first-of-type").fadeOut();
-        $("#slider ul li:nth-child(2)").fadeIn();
-        $("#slider ul").append($("#slider ul li:first-of-type"));
-    }, 2000);
+        $('#slider ul li:first-of-type')
+        .fadeOut(2000)
+        .next()
+        .fadeIn(1000)
+        .end()
+        .appendTo($("#slider ul"));
+
+        // 좀 더 짧게 코딩한다.
+        // $("#slider ul li:first-of-type").fadeOut();
+        // $("#slider ul li:nth-child(2)").fadeIn();
+        // $("#slider ul").append($("#slider ul li:first-of-type"));
+    }, 3000);
 });
